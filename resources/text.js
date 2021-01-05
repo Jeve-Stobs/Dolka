6d00f24721bf145134171a2b2a011c1fd8f068d8
Samuel Casey <samuelacasey@gmail.com>
2021-01-05 15:09:04 +0000

    Initial commit

---
+++ resources/text.js
@@ -0,0 +1,10 @@
+	$(document).ready(function() {
+	$(document).click(function (event) {    		
+    	$('.again-w').removeClass('unhide');
+    	$('.again-l').removeClass('unhide');
+	});
+	if(isCanvasSupported())
+    	cg.init()
+    else
+    	$(document.body).html("Your browser does not support HTML5, which is required to run this game.")
+})
